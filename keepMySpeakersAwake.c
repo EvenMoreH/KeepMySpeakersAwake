ad7fa378fde3215bac071145ae374b63ea314d7c
EvenMoreH <hubert.lewan@gmail.com>
2024-06-25 16:15:23 +0200

    refactor to have it run properly console less

--- keepMySpeakersAwake.c
+++ keepMySpeakersAwake.c
@@ -1,45 +1,30 @@
-#include <time.h>       // Copilot links it as required for randomizers
-#include <stdlib.h>     // StackOverflow says it is required to avoid "implicit declaration of function 'srand'" error while randomizing
-#include <stdio.h>      // Standard library
-#include <stdbool.h>    // Library required to use booleans
-#include <string.h>     // Unlocks string functions
-#include <limits.h>     // Allows to see max values for integers etc
-#include <math.h>       // Enables using math functions
-#include <windows.h>    // Unlocks windows functionalities
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <windows.h>
 
 DWORD frequency = 160;  // Base frequency
 DWORD duration = 21;    // More than 20ms was required for my speakers to register
-// One Beep is enough for these speakers, other required two beeps in sequence
+// One Beep is enough for these speakers, other required two beeps in sequence thus I leave below variables just in case
 // DWORD frequency2 = 200; 
 // DWORD duration2 = 22;
 
-
-int main() {            // Main function
-    FreeConsole();      // Thanks to <windows.h> library it should not create a console window
-
+int main() {
+    FreeConsole();          // Thanks to <windows.h> library it should not create a console window
     srand(time(NULL));      // Initialization for randomize that should be called ony once
-    
-    printf("\t\n*KEEP MY SPEAKERS AWAKE*\n\n");
-
     while (true)
     {
-        // Get time for speakers woken up message
-        time_t orig_format;
-        time(&orig_format);
-
-        int interval;       // interval variable for Sleep fuction in miliseconds
+        int interval;                               // interval variable for Sleep fuction must be in miliseconds
         interval = 700000 + rand() % 800000;        // Generates random value in milliseconds that fall between 11.67 and 13.33 minutes
 
-        // Beep uses DWORD values declared on top to produce frequency sound for specified duration (built in feature into windows)
+        // Beep uses DWORD values declared on top to produce frequency sound for specified duration (built in feature)
         Beep(frequency, duration);
         
-        // One Beep is enough for my speakers, other required two beeps in sequence
+        // One Beep is enough for my speakers, other required two beeps in sequence so I commented it out
         // Beep(frequency2, duration2);
 
-        printf ("> Speakers woken up at: %s\n",asctime(gmtime(&orig_format)));    // time format taken from include <time.h> document (outputs universal time)
-
-        // Sleep function uses interval argument to pause execution of a loop for specified in interval amount of seconds
         Sleep(interval);
     }
-    return 0;               // Ends main function
+    return 0;
 }
