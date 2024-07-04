c6b85f3fa6bf5a54923a5403227fe34e2592a6e0
EvenMoreH <hubert.lewan@gmail.com>
2024-07-04 22:12:29 +0200

    addressed randmizer bug

--- keepMySpeakersAwake.c
+++ keepMySpeakersAwake.c
@@ -7,7 +7,7 @@
 DWORD frequency = 160;  // Base frequency
 DWORD duration = 21;    // More than 20ms was required for my speakers to register
 // One Beep is enough for these speakers, other required two beeps in sequence thus I leave below variables just in case
-// DWORD frequency2 = 200; 
+// DWORD frequency2 = 200;
 // DWORD duration2 = 22;
 
 int main() {
@@ -16,11 +16,11 @@ int main() {
     while (true)
     {
         int interval;                               // interval variable for Sleep fuction must be in miliseconds
-        interval = 700000 + rand() % 800000;        // Generates random value in milliseconds that fall between 11.67 and 13.33 minutes
+        interval = 700000 + rand() % 100000;        // Generates random value in milliseconds that fall between 11.67 and 13.33 minutes
 
         // Beep uses DWORD values declared on top to produce frequency sound for specified duration (built in feature)
         Beep(frequency, duration);
-        
+
         // One Beep is enough for my speakers, other required two beeps in sequence so I commented it out
         // Beep(frequency2, duration2);
 
